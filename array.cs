91920f6bf53a659be7cb467deb546d185c61cc9d
JoseCarlosEnriqueGutierrez <rexbysmash@gmail.com>
2022-10-15 00:38:41 -0600

    Se agrega el mensaje en el main

--- array.cs
+++ array.cs
@@ -5,17 +5,18 @@ public class Program{
 
 	class aarray_multidimensional {
 
-		public static void array(string[,] num, double filas) {
+		public static void array(int[,] num, double filas) {
 			int i;
+			Console.Clear();
 			for (i=1;i<=filas;i++) {
+				Console.WriteLine("");
 				Console.WriteLine(num[i-1,0]+" + "+num[i-1,1]+" = "+num[i-1,2]);
 			}
 
 			StreamWriter BR = new StreamWriter (@"BRArray.txt");
 
 			for (i=1;i<=filas;i++) {
-				Console.WriteLine(num[i-1,0]+" + "+num[i-1,1]+" = "+num[i-1,2]);
-
+				Console.WriteLine("");
 				BR.WriteLine(num[i-1,0]+" + "+num[i-1,1]+" = "+num[i-1,2]);
 			}
 			BR.Close ();
@@ -27,20 +28,28 @@ public class Program{
 			int filas;
 			int i;
 			int j;
-			
+			Console.Clear();
+			Console.WriteLine("----------------------------------------");
+			Console.WriteLine("Bienvenidos a la pesentacion del grupo 2");
+			Console.WriteLine("----------------------------------------");
+			Console.WriteLine("");
 			Console.WriteLine("");
 			Console.WriteLine("Introduce las filas del array multidimensional");
 			Console.WriteLine("");
 			filas = int.Parse(Console.ReadLine());
 			columnas = 3;
-			string[,] num = new string[filas,columnas];
+			int[,] num = new int[filas,columnas];
 			for (i=1;i<=filas;i++) {
 				for (j=1;j<=columnas;j++) {
 					if ((j==columnas)) {
 						num[i-1,j-1] = num[i-1,0] + num[i-1,1];
 					} else {
+						Console.Clear();
+						Console.WriteLine("--------------------------------------------------");
 						Console.WriteLine("Introduce un numero para la FILA "+i+" COLUMNA " +j);
-						num[i-1,j-1] = (Console.ReadLine());
+						Console.WriteLine("--------------------------------------------------");
+						Console.WriteLine("");
+						num[i-1,j-1] = int.Parse(Console.ReadLine());
 					}
 				}
 			}
